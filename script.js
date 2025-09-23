0429a6b0730487f4622304f734b5a13b18d45499
Taha_Afzali <elfo.dp8787@gmail.com>
2025-09-24 00:31:36 +0330

    coding timers

--- script.js
+++ script.js
@@ -31,110 +31,37 @@ window.onclick = function (event) {
     }
   }
 };
+//* Varaibels
+let locale = navigator.language;
+let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const localeTimeZones = {
+  "fa-IR": "Asia/Tehran",
+  "en-US": "America/New_York",
+  "fr-FR": "Europe/Paris",
+};
 
 // Add click handler for options
 document.addEventListener("DOMContentLoaded", function () {
   const options = document.querySelectorAll(".language-dropdown option");
   options.forEach((option) => {
     option.addEventListener("click", function () {
-      console.log("Selected language:", this.value); // User will handle in JS
+      locale = option.value;
+      timeZone = localeTimeZones[option.value] || timeZone;
+      console.log("Selected language:", option.value, timeZone);
+      // User will handle in JS
+      updateDateTime();
       toggleDropdown();
     });
   });
 });
-//! my code
-/*
-const year = (year) => {
-  const now = new Date();
 
-  const locale = navigator.language;
-
-  year = new Intl.DateTimeFormat(locale, { year: "numeric" }).format(now);
-  NowYear.textContent = year;
-};
-year(NowYear);
-
-const month = (month) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  month = new Intl.DateTimeFormat(locale, { month: "2-digit" }).format(now);
-  NowMonth.textContent = month;
-};
-month(NowMonth);
-
-const day = (day) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  day = new Intl.DateTimeFormat(locale, { day: "2-digit" }).format(now);
-  NowDay.textContent = day;
-};
-day(NowDay);
-
-const hour = (hour) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  hour = new Intl.DateTimeFormat(locale, {
-    hour: "2-digit",
-    hour12: false,
-  }).format(now);
-  nowHuor.textContent = hour;
-};
-hour(nowHuor);
-
-const Min = (minute) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  minute = new Intl.DateTimeFormat(locale, { minute: "2-digit" }).format(now);
-  nowMin.textContent = minute;
-};
-Min(nowMin);
-
-const sec = (second) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  second = new Intl.DateTimeFormat(locale, { second: "2-digit" }).format(now);
-  nowSec.textContent = second;
-};
-sec(nowSec);
-
-const weekday = (weekday) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  weekday = new Intl.DateTimeFormat(locale, { weekday: "long" }).format(now);
-  dayName.textContent = weekday;
-};
-weekday(dayName);
-
-const monthNames = (monthsName) => {
-  const now = new Date();
-
-  const locale = navigator.language;
-
-  monthsName = new Intl.DateTimeFormat(locale, { month: "long" }).format(now);
-  monthName.textContent = monthsName;
-};
-monthNames(monthName);
-*/
 //! Clean Code ::
 
 const updateDateTime = () => {
+  const now = new Date();
   const dateFormat = (option) => {
     return new Intl.DateTimeFormat(locale, option).format(now);
   };
-  const locale = navigator.language;
-  const now = new Date();
   //! year num
   const year = dateFormat({ year: "numeric" });
   NowYear.textContent = year;
@@ -144,35 +71,41 @@ const updateDateTime = () => {
   //! day num
   const day = dateFormat({ day: "2-digit" });
   NowDay.textContent = day;
+  //! huor
   setInterval(() => {
-    //! huor num
-    // const hour = dateFormat({
-    //   hour: "2-digit",
-    //   hour12: false,
-    // });
-    const Time = new Date(new Date().getTime());
-    nowHuor.textContent = Time.getHours();
-  });
+    const Time = new Date();
+    nowHuor.textContent = new Intl.DateTimeFormat(locale, {
+      hour: "2-digit",
+      hour12: false,
+      timeZone,
+    }).format(Time);
+  }, 1000);
   //! weekDay
   const weekday = dateFormat({ weekday: "long" });
   dayName.textContent = weekday;
   //! namesMonth
   const monthsName = dateFormat({ month: "long" });
   monthName.textContent = monthsName;
+  //TODO Minute Timer
+  const MinuteTimer = () => {
+    const Time = new Date();
+    nowMin.textContent = new Intl.DateTimeFormat(locale, {
+      minute: "2-digit",
+      timeZone,
+    }).format(Time);
+  };
+  MinuteTimer();
+  setInterval(MinuteTimer, 1000);
 
-  //! mins num
-  setInterval(() => {
-    const Time = new Date(new Date().getTime());
-    nowMin.textContent = Time.getMinutes();
-  }, 1000);
-  setInterval(() => {
-    const Time = new Date(new Date().getTime());
-
-    //! sec num
-    const second = Time.getSeconds();
-    // console.log(now);
-    nowSec.textContent = second;
-  }, 1000);
-  // console.log(Time.getHours(), Time.getMinutes(), Time.getSeconds());
+  //TODO Second
+  const SecondTimer = () => {
+    const Time = new Date();
+    nowSec.textContent = new Intl.DateTimeFormat(locale, {
+      second: "2-digit",
+      timeZone,
+    }).format(Time);
+  };
+  SecondTimer();
+  setInterval(SecondTimer, 1000);
 };
 updateDateTime();
